e53f51364a0f4e4e203956dbae9b8a34e7c18fb1
LAPTOP-ML0416D6\68790 <je.maxime@hotmail.com>
2023-05-02 13:13:32 +1100

    feat : add @CrossOrigin

--- src/main/java/com/maxime/movieappbackend/controller/ContentController.java
+++ src/main/java/com/maxime/movieappbackend/controller/ContentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import com.maxime.movieappbackend.model.Content;
 import com.maxime.movieappbackend.response.Response;
 import com.maxime.movieappbackend.service.content.ContentService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/contents")
 public class ContentController {
